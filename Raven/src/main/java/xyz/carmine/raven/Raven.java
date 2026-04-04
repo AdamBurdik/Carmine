6db3aa8e39a7d8f11e61186ed347ddde206fe8ce
adamix <adamburdik9@gmail.com>
2026-04-04 13:23:45 +0200

    Add: player data saving to database
    The database is just redis for now

--- Raven/src/main/java/xyz/carmine/raven/Raven.java
+++ Raven/src/main/java/xyz/carmine/raven/Raven.java
@@ -12,22 +12,37 @@ import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.timer.TaskSchedule;
 import org.slf4j.Logger;
+import redis.clients.jedis.util.RedisInputStream;
 import xyz.carmine.raven.discord.DiscordService;
 import xyz.carmine.raven.exception.ServiceConnectionException;
+import xyz.carmine.raven.player.RavenPlayer;
 import xyz.carmine.raven.player.RavenPlayerProvider;
+import xyz.carmine.raven.player.data.PlayerData;
+import xyz.carmine.raven.player.data.service.PlayerDataRepository;
+import xyz.carmine.raven.player.data.service.PlayerDataService;
 
 @Slf4j
 public class Raven {
+    private static final String REDIS_HOST = "localhost";
+    private static final int REDIS_PORT = 6379;
+
     static InstanceContainer instance;
+
     static DiscordService discordService;
+    static PlayerDataService playerDataService;
 
     static void main() {
         try {
-            discordService = new DiscordService("localhost", 6379);
+            discordService = new DiscordService(REDIS_HOST, REDIS_PORT);
         } catch (ServiceConnectionException e) {
             log.error("Discord service failed: {}", e.getMessage());
         }
+
+        playerDataService = new PlayerDataService(
+                new PlayerDataRepository(REDIS_HOST, REDIS_PORT)
+        );
 
         MinecraftServer server = MinecraftServer.init(new Auth.Online());
 
@@ -44,7 +59,13 @@ public class Raven {
 
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
-            final Player player = event.getPlayer();
+            log.info("This is logged from: {}", Thread.currentThread().getName());
+
+            final RavenPlayer player = (RavenPlayer) event.getPlayer();
+
+            PlayerData data = playerDataService.getOrCreate(player.getUuid()).join();
+            player.setData(data);
+
             event.setSpawningInstance(instance);
             player.setRespawnPoint(new Pos(0, 2, 0));
         });
@@ -60,8 +81,21 @@ public class Raven {
             });
         }
 
+        // Save player data every 5 minutes
+        MinecraftServer.getSchedulerManager().scheduleTask(() -> {
+            log.info("Saving player data");
+            playerDataService.saveAllPlayers();
+        }, TaskSchedule.tick(1), TaskSchedule.minutes(5));
+
         server.start("localhost", 25565);
 
-        MinecraftServer.getSchedulerManager().buildShutdownTask(() -> discordService.shutdown());
+        MinecraftServer.getSchedulerManager().buildShutdownTask(() -> {
+            discordService.shutdown();
+
+            log.info("Shutting down... saving player data");
+            playerDataService.saveAllPlayers().join();
+            log.info("Player data saved. Closing connections.");
+            playerDataService.shutdown();
+        });
     }
 }
